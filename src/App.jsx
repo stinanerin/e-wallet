df5f5f52c8faf7bc79e7328f7262a382f2095de7
stormstina <stina.nerin@gmail.com>
2023-09-14 15:58:51 +0200

    Set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import {
+    createBrowserRouter,
+    Route,
+    createRoutesFromElements,
+    RouterProvider,
+} from "react-router-dom";
+
+import { Root } from "./pages/Root";
+import { Card } from "./pages/Card";
+import { AddCard } from "./pages/AddCard";
+
 function App() {
-    return <div>fe</div>;
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<Root />}>
+                <Route index element={<Card />} />
+                <Route path="add" element={<AddCard />} />
+            </Route>
+        )
+    );
+    return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
