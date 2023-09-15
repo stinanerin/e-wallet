9f475bafd909143b8aa281df9350da38fa0c30e8
stormstina <stina.nerin@gmail.com>
2023-09-15 13:48:33 +0200

    Refactor display of cc-num to  always have 16 elem

--- src/pages/AddCard.jsx
+++ src/pages/AddCard.jsx
@@ -1,10 +1,27 @@
 import { Form } from "../components/Form";
 import chipImage from "../assets/icons/chip2.svg";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { splitArrIntoChunks } from "../utils/helpers";
 
 export const AddCard = () => {
-    const [cardNum, setCardNum] = useState([]);
+    const [cardNum, setCardNum] = useState([1, 5, 6, 7, 4, 4, 4, 4]);
+
+    const [cardNumDisplay, setCardNumDisplay] = useState([]);
+
+    useEffect(() => {
+        // Shallow copy
+        const arr = [...cardNum];
+
+        // Add 'x' to the newArray until it reaches a length of 16
+        while (arr.length < 16) {
+            arr.push("X");
+        }
+
+        const numChunkArr = splitArrIntoChunks(arr, 4);
+        setCardNumDisplay(numChunkArr);
+    }, [cardNum]);
 
     return (
         <div>
@@ -12,8 +29,10 @@ export const AddCard = () => {
 
             <div className="h-32 px-4 py-6 bg-gray-900 rounded shadow-lg ">
                 <img src={chipImage} alt="" className="w-7" />
-                <p className="text-2xl text-text-contrast">
-                    {cardNum}xxxx xxxx xxxx
+                <p className="text-2xl text-text-contrast flex gap-3">
+                    {cardNumDisplay.map((chunk, i) => {
+                        return <span key={chunk + i}>{chunk}</span>;
+                    })}
                 </p>
             </div>
 
---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+export const splitArrIntoChunks = (arr, chunkSize) => {
+    const resultArray = arr.reduce((acc, curr, i) => {
+        const index = Math.floor(i / chunkSize);
+        if (!acc[index]) {
+            acc[index] = [];
+        }
+        acc[index].push(curr);
+        return acc;
+    }, []);
+
+    console.log(resultArray);
+    return resultArray;
+};
