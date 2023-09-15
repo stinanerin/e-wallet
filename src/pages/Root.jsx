652dcd5f57255545bb6f944de1585e0a15f75a7c
stormstina <stina.nerin@gmail.com>
2023-09-15 10:19:29 +0200

    Add header

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
-
+import { Header } from "../components/layout/Header";
 export const Root = () => {
     return (
-        <div>
-            <Outlet />
-        </div>
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+        </>
     );
 };
