d76844ac801f16c0d9c9499bf1d2cb71442b58ea
stormstina <stina.nerin@gmail.com>
2023-09-17 16:24:49 +0200

    Replace cc-code from add card with cc-component

--- src/features/cards/CreditCard.jsx
+++ src/features/cards/CreditCard.jsx
@@ -1,30 +1,48 @@
 import chipSvg from "../../assets/icons/chip2.svg";
 import signalSvg from "../../assets/icons/signal.svg";
 
-import { determineVendorSVG, splitArrIntoChunks } from "../../utils/helpers";
+import {
+    determineVendorSVG,
+    splitArrIntoChunks,
+    generateDisplayFormat,
+} from "../../utils/helpers";
 
 export const CreditCard = ({
     date: { month, year },
     number,
     vendor,
     user: { first, last },
+    useDisplayFormat = false,
 }) => {
+    const formattedNumber = useDisplayFormat
+        ? generateDisplayFormat(number, 16)
+        : number;
+
+    const formattedMonth = useDisplayFormat
+        ? generateDisplayFormat(month, 2)
+        : month;
+
+    const formattedYear = useDisplayFormat
+        ? generateDisplayFormat(year, 2)
+        : year;
+
     return (
         <div className=" px-4 py-6 bg-gray-400 rounded shadow-lg mb-5 max-w-md ">
             <div className="flex justify-between svg-icon">
                 <img src={signalSvg} alt="#" className="w-7 " />
-
-                <img
-                    src={determineVendorSVG(vendor)}
-                    alt="#"
-                    className="w-10 "
-                />
+                {vendor && (
+                    <img
+                        src={determineVendorSVG(vendor)}
+                        alt="#"
+                        className="w-10 "
+                    />
+                )}
             </div>
 
             <img src={chipSvg} alt="#" className="w-7 mb-3 mt-1" />
 
             <p className="text-2xl text-text-contrast flex gap-3">
-                {splitArrIntoChunks(number, 4).map((chunk, i) => {
+                {splitArrIntoChunks(formattedNumber, 4).map((chunk, i) => {
                     return <span key={chunk + i}>{chunk}</span>;
                 })}
             </p>
@@ -42,7 +60,7 @@ export const CreditCard = ({
                         valid thru
                     </p>
                     <p className="uppercase text-sm text-text-contrast text-end">
-                        {month}/{year}
+                        {formattedMonth}/{formattedYear}
                     </p>
                 </div>
             </div>
