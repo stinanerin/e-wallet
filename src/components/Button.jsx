53861b3713f03278da166f84e00528d4df760b7a
stormstina <stina.nerin@gmail.com>
2023-09-23 15:49:14 +0200

    Add danger btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,6 +10,10 @@ export const Button = ({ onClick, type, children, disabled }) => {
             buttonClasses +=
                 " border-2 border-primary-800 text-primary-800 hover:bg-primary-100 active:bg-primary-100 focus:bg-primary-10 focus:outline-none focus:ring focus:ring-primary-400";
             break;
+        case "danger":
+            buttonClasses +=
+                " border-2 border-danger-500 text-danger-500 hover:bg-danger-100 active:bg-danger-100 focus:bg-danger-100 focus:outline-none focus:ring focus:ring-danger-300";
+            break;
         default:
             break;
     }
