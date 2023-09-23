1998298b7c9d7889ab47d970bee3bc3572fabca9
stormstina <stina.nerin@gmail.com>
2023-09-23 20:12:15 +0200

    Break out gradientPicker & clear logs

--- src/components/form/GradientPicker.jsx
+++ src/components/form/GradientPicker.jsx
@@ -1,3 +1,36 @@
-export const GradientPicker = () => {
-    return <div>GradientPicker</div>;
+import { FormLabel } from "./FormLabel";
+import { FormInput } from "./FormInput";
+
+export const GradientPicker = ({
+    formData,
+    onChange,
+    id,
+    label,
+    type,
+    name,
+    required,
+    options,
+}) => {
+    return (
+        <div key={id}>
+            <FormLabel>{label}</FormLabel>
+            <div className="flex gap-10">
+                {options.map((option) => {
+                    console.log(option);
+                    return (
+                        <FormInput
+                            key={option}
+                            type={type}
+                            value={option}
+                            name={name}
+                            label={option}
+                            required={required}
+                            checked={formData[name] === option}
+                            onChange={onChange}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
